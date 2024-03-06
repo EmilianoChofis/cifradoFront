2ed2a41724e617f513cd9368a17bb2dea3f127c9
CrisSoven <20213tn044@utez.edu.mx>
2024-03-06 01:08:11 -0600

    [MODIFY] adaptación del formulario y pantalla de inicio con consumo de los servicios

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,5 @@
-<script setup>
-</script>
-
 <template>
   <div id="app">
     <router-view />
   </div>
 </template>
-
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,59 @@
-<script setup lang="ts">
-</script>
-
 <template>
-  <main>
+  <b-container class="mt-4">
+    <h1>Historial de compras</h1>
 
-  </main>
+    <b-button
+      variant="primary"
+      class="mt-2 mb-4"
+      :to="{ name: 'form' }"
+    >
+    Registrar compra
+  </b-button>
+
+    <b-row class="mt-4">
+      <b-col v-if="payments.length === 0">
+        <b-alert show variant="warning"> No se encontraron pagos registrados </b-alert>
+      </b-col>
+
+      <b-col v-for="payment in payments" :key="payment.id" md="4" class="mb-4">
+        <b-card :title="payment.nombre">
+          <b-card-text>
+            Propietario: {{ payment.nombre }} {{ payment.apellidos }}
+          </b-card-text>
+          <b-card-text> Monto: ${{ payment.monto }} MX </b-card-text>
+
+          <b-button
+            variant="primary"
+            :to="{ name: 'details', params: { id: payment.id } }"
+          >
+            Detalles de la compra
+          </b-button>
+        </b-card>
+      </b-col>
+    </b-row>
+  </b-container>
 </template>
+
+<script>
+import ServicePayment from "../services/ServicePayment.js";
+
+export default {
+  data() {
+    return {
+      payments: [],
+    };
+  },
+  created() {
+    this.getPayments();
+  },
+  methods: {
+    async getPayments() {
+      try {
+        this.payments = await ServicePayment.getPayments();
+      } catch (error) {
+        console.error(error);
+      }
+    },
+  },
+};
+</script>
