881156a04bae29a7a774a14b973a7013b2257517
EmilianoChofis <20213tn128@utez.edu.mx>
2024-03-05 20:44:32 -0600

    first commit

---
+++ src/router/index.js
@@ -0,0 +1,24 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+
+Vue.use(VueRouter)
+
+const router = new VueRouter({
+  mode: 'history',
+  base: import.meta.env.BASE_URL,
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView
+    },
+    {
+      path: '/form',
+      name: 'form',
+      component: () => import('../views/Form.vue')
+    }
+  ]
+})
+
+export default router
---
+++ src/views/Form.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+</script>
+
+<template>
+  <div id="form">
+
+  </div>
+</template>
+
+<style scoped>
+
+</style>
