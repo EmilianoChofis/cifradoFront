8165476aa8ab0a6e9a1726d6ed9c77bf6e4bd22f
CrisSoven <20213tn044@utez.edu.mx>
2024-03-06 01:11:31 -0600

    [ADD] pantalla de detalles de las compras y modulo de servicios

---
+++ src/views/Details.vue
@@ -0,0 +1,56 @@
+<template>
+  <b-container class="mt-4">
+    <h1>Detalles de la compra</h1>
+
+    <b-row class="mt-4">
+      <b-col>
+        <b-card>
+          <b-card-text>
+            <b-form-group label="Nombre">
+              <b-form-input v-model="payment.nombre" readonly></b-form-input>
+            </b-form-group>
+            <b-form-group label="Apellidos">
+              <b-form-input v-model="payment.apellidos" readonly></b-form-input>
+            </b-form-group>
+            <b-form-group label="Monto">
+              <b-form-input v-model="payment.monto" readonly></b-form-input>
+            </b-form-group>
+            <b-form-group label="Fecha">
+              <b-form-input v-model="payment.fecha" readonly></b-form-input>
+            </b-form-group>
+          </b-card-text>
+        </b-card>
+      </b-col>
+    </b-row>
+  </b-container>
+</template>
+
+<script>
+import ServicePayment from "../services/ServicePayment.js";
+
+export default {
+  data() {
+    return {
+      payment: {
+        nombre: "",
+        apellidos: "",
+        monto: 0,
+        fecha: "",
+      },
+    };
+  },
+  mounted() {
+    this.getPayment();
+  },
+    methods: {
+        async getPayment() {
+        try {
+            const id = this.$route.params.id;
+            // this.payment = await ServicePayment.getPayment(id);
+        } catch (error) {
+            console.error(error);
+        }
+        },
+    },
+};
+</script>
